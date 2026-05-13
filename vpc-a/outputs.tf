a49722fccb88431b152f80e592300f1fe3066d7c
DTrain <darrellcarley2@gmail.com>
2026-05-13 01:04:33 -0400

    add vpc-a: outputs.tf and peerings.tf to semi-final configs. Had to manually rollback due to 'terraform destroy' attempts

--- vpc-a/outputs.tf
+++ vpc-a/outputs.tf
@@ -33,7 +33,7 @@ output "private_ip" {
   value        = aws_instance.dev.*.private_ip
 }
 
-output "peering_connection_id" {
-  description  = "ID of VPC peering connection"
-  value        = aws_vpc_peering_connection.dev_to_test.id
-}
+#output "peering_connection_id" {
+#  description  = "ID of VPC peering connection"
+#  value        = aws_vpc_peering_connection.dev_to_test.id
+#}
--- vpc-a/peering.tf
+++ vpc-a/peering.tf
@@ -9,29 +9,29 @@ data "terraform_remote_state" "vpc_b" {
 
 }
 
-resource "aws_vpc_peering_connection" "dev_to_test" {
+#resource "aws_vpc_peering_connection" "dev_to_test" {
 
   # Peer VPC details
-  peer_owner_id  = data.aws_caller_identity.current.account_id
-  peer_vpc_id    = data.terraform_remote_state.vpc_b.outputs.vpc_id
+#  peer_owner_id  = data.aws_caller_identity.current.account_id
+#  peer_vpc_id    = data.terraform_remote_state.vpc_b.outputs.vpc_id
 
   # Current VPC details
-  vpc_id         = aws_vpc.dev.id
-  auto_accept    = true
+#  vpc_id         = aws_vpc.dev.id
+#  auto_accept    = true
 
-  tags    = {
-    Name  = "dev-to-test"
-  }
+#  tags    = {
+#    Name  = "dev-to-test"
+#  }
 
-}
+#}
 
-resource "aws_route" "route_to_test" {
+#resource "aws_route" "route_to_test" {
   
   # Source route table ID
-  route_table_id            = aws_route_table.public.id
+#  route_table_id            = aws_route_table.public.id
 
   # Destination CIDR / PCX ID
-  destination_cidr_block    = data.terraform_remote_state.vpc_b.outputs.vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.dev_to_test.id
+#  destination_cidr_block    = data.terraform_remote_state.vpc_b.outputs.vpc_cidr
+#  vpc_peering_connection_id = aws_vpc_peering_connection.#dev_to_test.id
 
-}
+#}
